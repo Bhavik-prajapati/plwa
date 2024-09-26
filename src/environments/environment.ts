2b32c23b398722197dcbb8e5745196606f0c28df
Bhavik-prajapati <bhavikprajapati4244@gmail.com>
2024-09-26 17:06:31 +0000

    add

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.ts (for development)
+export const environment = {
+    production: false,
+    razorpayKey: '12fdjfjd3' // Replace with your Razorpay Key ID for development
+  };
+  
